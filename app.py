40004463769eccc1bf35852861aae5b70cef91e6
chacha <120308666+hyebin-mkt@users.noreply.github.com>
2025-08-12 16:20:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from pathlib import Path
 import streamlit as st
 from PIL import Image, ImageOps
 
-APP_TITLE = "Snap2Size | 이미지 한 장으로 마이다스 이벤트 필요 사이즈를 한방에 추출하세요.⚡"
+APP_TITLE = "⚡원샷원킬 | 이미지 하나로 이벤트 필요 사이즈를 한번에 추출하세요 "
 VALID_EXTS = {".jpg", ".jpeg", ".png", ".webp", ".bmp", ".tiff"}
 
 PRESETS = [
